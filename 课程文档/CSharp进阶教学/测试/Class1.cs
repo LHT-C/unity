532ace1fa036aef039ad44d92e34b74cc402857c
aheiyanP <15363245680@163.com>
2023-02-12 17:23:39 +0800

    上传 C#进阶 课程文档

---
+++ 课程文档/CSharp进阶教学/测试/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrTang
+{
+    class MyCustomAttribute : Attribute
+    {
+
+    }
+
+    struct Position
+    {
+        public int x;
+        public int y;
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Player
+    {
+        [MyCustom()]
+        public string name;
+
+        public int hp;
+        public int atk;
+        public int def;
+        public Position pos;
+    }
+}
