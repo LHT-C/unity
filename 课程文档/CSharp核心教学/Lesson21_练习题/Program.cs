d2f38cb14d01d81c1532bbf88147e80070438081
aheiyanP <15363245680@163.com>
2023-02-12 12:23:24 +0800

    上传 C#核心 课程文档

---
+++ 课程文档/CSharp核心教学/Lesson21_练习题/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UI;
+using Graph;
+
+#region 练习题一
+//请说明关键词using有什么作用
+//using是用来引用命名空间
+#endregion
+
+#region 练习题二
+//有两个命名空间，UI(用户界面)和Graph（图表）
+//两个命名空间中都有一个Image类
+//请在主函数中实例化两个不同命名空间中的Image对象
+namespace UI
+{
+    class Image
+    {
+
+    }
+}
+
+namespace Graph
+{
+    class Image
+    {
+
+    }
+}
+
+#endregion
+
+namespace Lesson21_练习题
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("命名空间练习题");
+
+            UI.Image uiImg = new UI.Image();
+            Graph.Image grImg = new Graph.Image();
+        }
+    }
+}
---
+++ 课程文档/CSharp核心教学/Lesson8_练习题/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lesson8_练习题
+{
+    #region 练习题
+    //写一个用于数学计算的静态类
+    //该类中提供计算圆面积，圆周长，矩形面积，矩形周长，取一个数的绝对值 等方法
+    static class MathTool
+    {
+        public static float PI = 3.14f;
+        /// <summary>
+        /// 计算圆的面积
+        /// </summary>
+        /// <param name="r">半径</param>
+        /// <returns>面积</returns>
+        public static float CalcCircularArea(float r)
+        {
+            return PI * r * r;
+        }
+
+        public static float CalcCircularLength(float r)
+        {
+            return 2 * PI * r;
+        }
+
+        public static float CalcRectArea(float w, float h)
+        {
+            return w * h;
+        }
+
+        public static float CalcRectLength(float w, float h)
+        {
+            return 2 * w + 2 * h;
+        }
+
+        public static float GetABS(float value)
+        {
+            if( value < 0 )
+            {
+                return -value;
+            }
+            return value;
+        }
+    }
+    #endregion
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(MathTool.GetABS(-7));
+            Console.WriteLine(MathTool.CalcRectLength(2, 3));
+        }
+    }
+}
